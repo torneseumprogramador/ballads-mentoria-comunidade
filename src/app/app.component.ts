bbd73572794a0506244369b63166d47e647fce71
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-01-06 22:41:51 -0300

    iniciando o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Entrada', url: '/folder/Entrada', icon: 'mail' },
+    { title: 'Saida', url: '/folder/Saida', icon: 'paper-plane' },
+    { title: 'Favoritos', url: '/folder/Favoritos', icon: 'heart' },
+    { title: 'Arquivados', url: '/folder/Arquivados', icon: 'archive' },
+    { title: 'Lixeira', url: '/folder/Lixeira', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
