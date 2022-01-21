4b308d4299357840a0e49601391405a2b20894aa
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-01-21 16:04:32 -0300

    alterando código para trazer lista

---
+++ src/app/models/music.ts
@@ -0,0 +1,9 @@
+export class Music{
+  constructor() { }
+
+  public id:number;
+  public title: string;
+  public path_of_music: string;
+  public description: string;
+  public author: string;
+}
--- src/app/music/folder.page.ts
+++ src/app/music/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Music } from '../models/music';
 import { Menu } from '../services/menu';
 
 @Component({
@@ -10,6 +11,7 @@ import { Menu } from '../services/menu';
 export class FolderPage implements OnInit {
   public title: string;
   public description: string;
+  public musics = [];
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
@@ -17,6 +19,30 @@ export class FolderPage implements OnInit {
     const slug = this.activatedRoute.snapshot.paramMap.get('id');
     this.title = Menu.titleBySlug(slug);
     this.description = Menu.descriptionBySlug(slug);
+
+    const music = new Music();
+    music.id = 1;
+    music.title = "Quando eu estou aqui";
+    music.description = "Musica do Roberto Carlos";
+    music.author = "Roberto Carlos";
+    music.path_of_music = "...";
+    this.musics.push(music);
+
+    const music2 = new Music();
+    music2.id = 2;
+    music2.title = "Um lugar ao sol";
+    music2.description = "Musica de Charlie Brown Jr";
+    music2.path_of_music = "...";
+    music2.author = "Charlie Brown Jr";
+    this.musics.push(music2);
+
+    const music3 = new Music();
+    music3.id = 3;
+    music3.title = "Vira-vira";
+    music3.description = "Musica do Mamonas Assassinas";
+    music3.path_of_music = "...";
+    music3.author = "Mamonas Assassinas";
+    this.musics.push(music3);
   }
 
 }
